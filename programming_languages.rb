28faa5ff0c8133d468b82098e4bf8ab6153fad31
Manu Ticusan <manuelticusan@yahoo.com>
2019-02-11 23:58:16 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+new = {}
+  languages.each do |style, lang|
+    new = new.merge(lang)
+    lang.each do |name, type|
+      new[name][:style] = []
+    end
+  end
+  new.keys.each do |key|
+    if languages[languages.keys.first].include?(key) && languages[languages.keys.last].include?(key)
+      new[key][:style] = languages.keys
+      elsif languages[languages.keys.first].include?(key)
+        new[key][:style] = [languages.keys.first]
+      else
+        new[key][:style] = [languages.keys.last]
+    end
+  end
+new
 end
